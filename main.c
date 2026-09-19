850357398f6db7d282cea547add885a8bdb0be2a
pulinduwanigasooriya30804 <pulinduwanigasooriya30804@gmail.com>
2026-09-19 22:12:00 +0530

    Initialize hospital bed availability

--- main.c
+++ main.c
@@ -77,9 +77,27 @@ double waitingTime[MAX_PATIENTS];
 
 int bedOccupancy[MAX_WARDS][MAX_BEDS];
 
+void initializeBeds();
 
 int main()
 {
+    int choice;
+
+    initializeBeds();
+
     printf("==========Smart Hospital System==========\n");
     return 0;
 }
+
+void initializeBeds()
+{
+    int i, j;
+    for(i=0; i< MAX_WARDS; i++)
+    {
+        for(j=0; j < MAX_BEDS; j++)
+        {
+            bedOccupancy[i][j] = 0;
+        }
+    }
+}
+
