94c7d6d565803383ba39b3bb1f6f3b3ed25832f8
pulinduwanigasooriya30804 <pulinduwanigasooriya30804@gmail.com>
2026-09-19 17:14:44 +0530

    Add hospital ward information

--- main.c
+++ main.c
@@ -35,6 +35,27 @@ const int dailyPatientCap[4] = {
     10
 };
 
+const char *wardName[4] = {
+    "General Ward",
+    "Paediatric Ward",
+    "Surgical Ward",
+    "ICU (Intensive Care Unit)"
+};
+
+const double wardDailyRate[4] = {
+    3000.00,
+    6000.00,
+    12000.00,
+    25000.00
+};
+
+const int wardCapacity[4] = {
+    20,
+    10,
+    10,
+    5
+};
+
 int main()
 {
     printf("==========Smart Hospital System==========\n");
