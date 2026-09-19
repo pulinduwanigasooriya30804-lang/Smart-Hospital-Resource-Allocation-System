ada0dfaf6727c776dde23c1176406516d4192362
pulinduwanigasooriya30804 <pulinduwanigasooriya30804@gmail.com>
2026-09-19 19:54:26 +0530

    Add patient data storage

--- main.c
+++ main.c
@@ -56,6 +56,25 @@ const int wardCapacity[4] = {
     5
 };
 
+int patientCount = 0;
+
+char patientName[MAX_PATIENTS][MAX_NAME];
+int patientAge[MAX_PATIENTS];
+int urgencyLevel[MAX_PATIENTS];
+int patientSpecialty[MAX_PATIENTS];
+int admitted[MAX_PATIENTS];
+int patientWard[MAX_PATIENTS];
+int admittedDays[MAX_PATIENTS];
+int assignedBed[MAX_PATIENTS];
+
+double baseFee[MAX_PATIENTS];
+double surcharge[MAX_PATIENTS];
+double wardCost[MAX_PATIENTS];
+double grossTotal[MAX_PATIENTS];
+double discount[MAX_PATIENTS];
+double finalAmount[MAX_PATIENTS];
+double waitingTime[MAX_PATIENTS];
+
 int main()
 {
     printf("==========Smart Hospital System==========\n");
