3deb6183a56dbfbb511916453407e4582993b72d
pulinduwanigasooriya30804 <pulinduwanigasooriya30804@gmail.com>
2026-09-20 16:10:59 +0530

    Improve performance report

--- report.c
+++ report.c
@@ -4,9 +4,38 @@
 
 void generateReport()
 {
+    int i;
+    int admittedCount = 0;
+    int outpatientCount = 0;
+    double totalRevenue = 0.0;
+    double averageBill = 0.0;
+
+    for(i = 0; i < patientCount; i++)
+    {
+        if(admitted[i] == 1)
+        {
+            admittedCount++;
+        }
+        else
+        {
+            outpatientCount++;
+        }
+
+        totalRevenue += finalAmount[i];
+    }
+
+    if(patientCount > 0)
+    {
+        averageBill = totalRevenue / patientCount;
+    }
+
     printf("\n========== HOSPITAL PERFORMANCE REPORT ==========\n");
 
     printf("Total Registered Patients : %d\n", patientCount);
+    printf("Admitted Patients         : %d\n", admittedCount);
+    printf("Outpatients               : %d\n", outpatientCount);
+    printf("Total Revenue             : Rs. %.2f\n", totalRevenue);
+    printf("Average Bill Amount       : Rs. %.2f\n", averageBill);
 
-    printf("===============================================\n");
+    printf("=================================================\n");
 }
